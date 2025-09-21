22266714ea8110d2687971b14352d497280405a9
Jeannie Fallon <jeanniefallon@gmail.com>
2025-09-21 13:14:04 -0400

    code clean-up

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-from __future__ import annotations
-
 import sys
 import pathlib
 import argparse
@@ -22,11 +20,11 @@ DTYPES: Final = {
 """Logger & error hanlder to standardize script output"""
 
 
-def log(msg: string) -> None:
+def log(msg: str) -> None:
     print(f"\n>>> {msg}")
 
 
-def err(msg: string) -> None:
+def err(msg: str) -> None:
     print(f"\n!!! {msg}", file=sys.stderr)
 
 
@@ -35,30 +33,30 @@ def err(msg: string) -> None:
 
 def ingest(infile: pathlib.Path) -> pd.DataFrame:
     try:
-        data_frame = pd.read_csv(infile, dtype=DTYPES, parse_dates=["ts"])
+        df = pd.read_csv(infile, dtype=DTYPES, parse_dates=["ts"])
     except pd.errors.EmptyDataError:
         raise ValueError(f"{infile} is empty or has no columns")
 
-    if data_frame.empty:
+    if df.empty:
         raise ValueError(f"{infile} contained headers but no rows of data")
 
-    missing = COLS - set(data_frame.columns)
+    missing = COLS - set(df.columns)
     if missing:
         raise ValueError(f"Missing required columns: {missing}")
 
-    return data_frame
+    return df
 
 
 """Call ingest, manage outdir creation, and write data frame to outfile"""
 
 
 def run(infile: pathlib.Path, outdir: pathlib.Path) -> pathlib.Path:
-    data_frame = ingest(infile)
-    log(f"Ingested {len(data_frame):,} rows with columns {list(data_frame.columns)}")
+    df = ingest(infile)
+    log(f"Ingested {len(df):,} rows with columns {list(df.columns)}")
 
     outdir.mkdir(parents=True, exist_ok=True)
     outfile = outdir / f"ingested_{TIMESTAMP}.csv"
-    data_frame.to_csv(outfile, index=False)
+    df.to_csv(outfile, index=False)
 
     return outfile
 
